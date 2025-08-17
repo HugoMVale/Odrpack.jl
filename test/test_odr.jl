0790d8864f24bbdd69a684e3318724e2a03bb218
HugoMVale <57530119+HugoMVale@users.noreply.github.com>
2025-08-17 12:02:15 +0200

    extend test suite

--- test/test_odr.jl
+++ test/test_odr.jl
@@ -18,19 +18,19 @@ end
 function create_case1()
     # m=1, q=1
     function f!(x::Vector{Float64}, beta::Vector{Float64}, y::Vector{Float64})
-        y .= beta[1] .+ beta[2] .* x .+ beta[3] .* x .^ 2 .+ beta[4] .* x .^ 3
+        y .= beta[1] .+ beta[2] * x .+ beta[3] * x .^ 2 .+ beta[4] * x .^ 3
         return nothing
     end
 
     beta_star = [1.0, -2.0, 0.1, -0.1]
-    x = collect(range(-10.0, 10.0, length=21))
-    y = zeros(size(x, 1))
-    f!(x, beta_star, y)
+    xdata = collect(range(-10.0, 10.0, length=21))
+    ydata = zeros(size(xdata, 1))
+    f!(xdata, beta_star, ydata)
 
-    add_noise!(x, 5e-2)
-    add_noise!(y, 10e-2)
+    add_noise!(xdata, 5e-2)
+    add_noise!(ydata, 10e-2)
 
-    return (f=f!, xdata=x, ydata=y, beta0=zeros(length(beta_star)))
+    return (f=f!, xdata=xdata, ydata=ydata, beta0=zeros(length(beta_star)))
 end
 
 function create_case2()
@@ -55,13 +55,13 @@ end
 function create_case3()
     # m=3, q=2
     function f!(x::Matrix{Float64}, beta::Vector{Float64}, y::Matrix{Float64})
-        y[:, 1] .= (beta[1] .* x[:, 1]) .^ 3 .+ x[:, 2] .^ beta[2] .+ exp.(x[:, 3] ./ 2)
-        y[:, 2] .= (beta[3] .* x[:, 1]) .^ 2 .+ x[:, 2] .^ beta[2]
+        y[:, 1] .= (beta[1] * x[:, 1]) .^ 3 .+ x[:, 2] .^ beta[2] .+ exp.(x[:, 3] ./ 2)
+        y[:, 2] .= (beta[3] * x[:, 1]) .^ 2 .+ x[:, 2] .^ beta[2]
         return nothing
     end
 
     beta_star = [1.0, 2.0, 3.0]
-    x1 = collect(range(-1.0, stop=1.0, length=31))
+    x1 = collect(range(-1.0, 1.0, length=31))
     xdata = hcat(x1, exp.(x1), (x1 .^ 2))
     ydata = zeros(size(xdata, 1), 2)
     f!(xdata, beta_star, ydata)
@@ -203,14 +203,14 @@ end # "beta-related"
     fix = [2]
     fix_x[fix] .= true
     sol = odr_fit(case3..., fix_x=fix_x)
-    @test sol.delta[:, fix] ≈ zeros(size(sol.delta, 1))
+    @test sol.delta[:, fix] ≈ zeros(size(sol.delta[:, fix]))
 
     # fix some x, broadcast (n,)
     fix_x = falses(size(case3.xdata, 1))
     fix = [2, 7, 13]
     fix_x[fix] .= true
     sol = odr_fit(case3..., fix_x=fix_x)
-    @test sol.delta[fix, :] ≈ zeros(length(fix), size(sol.delta, 2))
+    @test sol.delta[fix, :] ≈ zeros(size(sol.delta[fix, :]))
 
     # fix all x (n,)
     fix_x = trues(size(case1.xdata))
@@ -220,6 +220,7 @@ end # "beta-related"
     # fix all x (n, m)
     fix_x = trues(size(case3.xdata))
     sol = odr_fit(case3..., fix_x=fix_x)
+    @test sol.delta ≈ zeros(size(sol.delta))
 
     # user-defined step_delta
     sol3 = odr_fit(case3...)
@@ -234,9 +235,7 @@ end # "beta-related"
     # user-defined scale_delta
     sol3 = odr_fit(case3...)
     @test sol3.success
-    for shape in (size(case3.xdata),
-        size(case3.xdata, 1),
-        size(case3.xdata, 2))
+    for shape in (size(case3.xdata), size(case3.xdata, 1), size(case3.xdata, 2))
         scale_delta = fill(10.0, shape)
         sol = odr_fit(case3..., scale_delta=scale_delta)
         @test sol.success
@@ -433,3 +432,150 @@ end # "weight_y"
 
 end # "parameters"
 
+@testset "rptfile-and-errfile" begin
+
+    # write to report file
+    for (report, rptsize) in zip(["none", "short"], [0, 2600])
+        rptfile = tempname()
+        odr_fit(case1..., report=report, rptfile=rptfile)
+        @test isfile(rptfile)
+        @test abs(filesize(rptfile) - rptsize) < 200
+    end
+
+    # write to error file
+    errfile = tempname()
+    odr_fit(case1..., report="short", errfile=errfile)
+    @test isfile(errfile) # && filesize(errfile) > 0
+
+    # write to report and error file
+    rptfile = tempname()
+    errfile = tempname()
+    odr_fit(case1..., report="short", rptfile=rptfile, errfile=errfile)
+    @test isfile(rptfile) && filesize(rptfile) > 2500
+    @test isfile(errfile) # && filesize(errfile) > 0
+
+end # "rptfile-and-errfile"
+
+@testset "OLS" begin
+    sol1 = odr_fit(case1..., task="OLS")
+    sol2 = odr_fit(case1..., weight_x=1e100)
+
+    @test all(isapprox.(sol1.beta, sol2.beta, atol=1e-10))
+    @test all(isapprox.(sol1.delta, zeros(size(sol1.delta)), atol=1e-90))
+end # "OLS"
+
+@testset "implicit-model" begin
+
+    # "odrpack's example2"
+    xdata = [0.50 -0.12;
+        1.20 -0.60;
+        1.60 -1.00;
+        1.86 -1.40;
+        2.12 -2.54;
+        2.36 -3.36;
+        2.44 -4.00;
+        2.36 -4.75;
+        2.06 -5.25;
+        1.74 -5.64;
+        1.34 -5.97;
+        0.90 -6.32;
+        -0.28 -6.44;
+        -0.78 -6.44;
+        -1.36 -6.41;
+        -1.90 -6.25;
+        -2.50 -5.88;
+        -2.88 -5.50;
+        -3.18 -5.24;
+        -3.44 -4.86]
+    ydata = fill(0.0, size(xdata, 1))
+    beta0 = [-1.0, -3.0, 0.09, 0.02, 0.08]
+
+    beta_ref = [-9.99380462E-01, -2.93104890E+00, 8.75730642E-02, 1.62299601E-02, 7.97538109E-02]
+
+    function f!(x::Matrix{Float64}, beta::Vector{Float64}, y::Vector{Float64})
+        v = @view x[:, 1]
+        h = @view x[:, 2]
+        y .= beta[3] * (v .- beta[1]) .^ 2 .+ 2 * beta[4] * (v .- beta[1]) .* (h .- beta[2]) .+ beta[5] * (h .- beta[2]) .^ 2 .- 1
+        return nothing
+    end
+
+    sol = odr_fit(f!, xdata, ydata, beta0, task="implicit-ODR")
+    @test all(isapprox.(sol.beta, beta_ref, rtol=1e-5))
+
+end # "implicit-model"
+
+@testset "OdrStop-exception" begin
+    xdata = [1.0, 2.0, 3.0, 4.0]
+    ydata = [1.0, 2.0, 3.0, 4.0]
+    beta0 = [1.0, 1.0]
+
+    function f!(x::Vector{Float64}, beta::Vector{Float64}, y::Vector{Float64})
+        if beta[1] > 0
+            throw(OdrStop("Oops!"))
+        end
+        y .= beta[1] * exp.(beta[2] * x)
+        return nothing
+    end
+
+    sol = odr_fit(f!, xdata, ydata, beta0)
+    @test !sol.success
+    @test sol.info == 52000
+
+end # "OdrStop-exception
+
+@testset "jac_beta-and-jac_x" begin
+
+    # odrpack's example5
+    xdata = [0.982, 1.998, 4.978, 6.01]
+    ydata = [2.7, 7.4, 148.0, 403.0]
+    beta0 = [2.0, 0.5]
+    bounds = ([0.0, 0.0], [10.0, 0.9])
+
+    beta_ref = [1.63337602, 0.9]
+    delta_ref = [-0.36886137, -0.31273038, 0.029287, 0.11031505]
+
+    function f!(x::Vector{Float64}, beta::Vector{Float64}, y::Vector{Float64})
+        y .= beta[1] * exp.(beta[2] * x)
+        return nothing
+    end
+
+    function jac_beta!(x::Vector{Float64}, beta::Vector{Float64}, jacb::Array{Float64})
+        jacb[:, 1, 1] .= exp.(beta[2] * x)
+        jacb[:, 2, 1] .= beta[1] * x .* exp.(beta[2] * x)
+        return nothing
+    end
+
+    function jac_x!(x::Vector{Float64}, beta::Vector{Float64}, jacx::Array{Float64})
+        jacx[:, 1, 1] .= beta[1] * beta[2] * exp.(beta[2] * x)
+        return nothing
+    end
+
+    # ODR without jacobian
+    for diff_scheme in ["forward", "central"]
+        sol = odr_fit(f!, xdata, ydata, beta0, bounds=bounds, diff_scheme=diff_scheme)
+        @test all(isapprox.(sol.beta, beta_ref, rtol=1e-5))
+        @test all(isapprox.(sol.delta, delta_ref, atol=1e-5))
+    end
+
+    # ODR with jacobian
+    sol = odr_fit(f!, xdata, ydata, beta0, bounds=bounds,
+        (jac_beta!)=jac_beta!, (jac_x!)=jac_x!)
+    @test all(isapprox.(sol.beta, beta_ref, rtol=1e-6))
+    @test all(isapprox.(sol.delta, delta_ref, atol=1e-6))
+
+    # OLS with jacobian
+    sol1 = odr_fit(f!, xdata, ydata, beta0, weight_x=1e100)
+    sol2 = odr_fit(f!, xdata, ydata, beta0, (jac_beta!)=(jac_beta!), task="OLS")
+    @test all(isapprox.(sol2.beta, sol1.beta, rtol=1e-6))
+    @test all(isapprox.(sol2.delta, zeros(length(xdata))))
+
+    # missing jac_beta
+    @test_throws ArgumentError odr_fit(f!, xdata, ydata, beta0, (jac_x!)=jac_x!)
+
+    # missing jac_x
+    @test_throws ArgumentError odr_fit(f!, xdata, ydata, beta0, (jac_beta!)=jac_beta!)
+
+    # invalid diff_scheme
+    @test_throws ArgumentError odr_fit(f!, xdata, ydata, beta0, diff_scheme="invalid")
+
+end # "jac_beta-and-jac_x"
